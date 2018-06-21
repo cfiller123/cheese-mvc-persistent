fb2705f2de3abefc637cd1e6ce261aa85ec80bf2
cfiller123 <carlfiller@gmail.com>
2018-06-21 18:45:03 -0500

    Finished - up to "Add a Menu" part

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,14 @@ public class MenuController {
 
     @Autowired
     private CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "My Menus");
+
+        return "menu/index";
+    }
 
 }
