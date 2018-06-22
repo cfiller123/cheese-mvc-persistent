a16525c1ebbd91f1e18bbd1d7e9c74339bf38c3a
cfiller123 <carlfiller@gmail.com>
2018-06-22 10:04:03 -0500

    Setting up "View a Menu"

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
@@ -24,6 +30,27 @@ public class MenuController {
         model.addAttribute("title", "My Menus");
 
         return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String addSubmit(@ModelAttribute @Valid Menu menu, Errors errors, Model model) {
+
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+        }
+
+        menuDao.save(menu);
+        return "redirect:/view/" + menu.getId();
     }
 
 }
